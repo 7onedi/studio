655ff3707bb8698a988ad8ffd54596a2e4f947b8
Serega <108889500+Seregamk@users.noreply.github.com>
2026-02-27 12:09:21 +0200

    Config docker setup & DB connection with Prisma ORM (#66)
    * Config docker setup & DB connection with Prisma ORM
    
    * Add .env example (#67)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
 
+echo "MySQL is up - running migrations..."
+
+npx prisma migrate deploy
+
 echo "NODE_ENV=$NODE_ENV"
 
 if [ "$NODE_ENV" = "production" ]; then
   exec pnpm run start
 else
+  npx prisma studio --browser none &
   exec pnpm run dev
 fi
