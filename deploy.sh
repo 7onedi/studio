5a16fca2e630caea54ffd4574acd96da855e9dac
Serega <108889500+Seregamk@users.noreply.github.com>
2026-03-19 13:54:39 +0200

    Fix docker config (#100)

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 git pull origin main
 
 # 1) Спочатку білдимо (без даунтайму)
-docker compose build --no-cache
+docker compose build
 
 # 2) Потім піднімаємо/оновлюємо (compose сам перезапустить що треба)
 docker compose up -d --remove-orphans
