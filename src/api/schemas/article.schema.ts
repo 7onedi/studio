050743fc041f292dfd7a3f93129713d72059f869
Seregamk <serega.mk2@gmail.com>
2026-03-21 12:26:44 +0200

    Add gradient enum to article

--- src/api/schemas/article.schema.ts
+++ src/api/schemas/article.schema.ts
@@ -3,11 +3,13 @@ import { sl } from "zod/v4/locales";
 
 export const ArticleLangEnum = z.enum(["UK", "EN", "PL", "LT" ,"RO"]);
 export const ArticleSliderEnum = z.enum(["NONE", "SLIDER_1", "SLIDER_2", "SLIDER_3"]).optional();
+export const ArticleGradient = z.enum(["NONE", "GRADIENT_1", "GRADIENT_2"]).optional();
 
 export const createArticleSchema = z.object({
   title: z.string().min(3),
   lang: ArticleLangEnum,
   slider: ArticleSliderEnum,
+  gradient: ArticleGradient,
   body: z.any(), // JSON від EditorJS
   authorName: z.string().min(2),
   categoryId: z.number(),
@@ -24,6 +26,7 @@ export const updateArticleSchema = z.object({
   title: z.string().min(3).optional(),
   lang: ArticleLangEnum.optional(),
   slider: ArticleSliderEnum.optional(),
+  gradient: ArticleGradient.optional(),
   body: z.any().optional(), // JSON від EditorJS
   authorName: z.string().min(2).optional(),
   categoryId: z.number().optional(),
